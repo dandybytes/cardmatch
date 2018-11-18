20241845d2689229011b4007d4262d560f035fcb
dandybytes <dandybytes@gmx.com>
2018-11-18 16:30:27 +0200

    enabled color change on card reveal

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 const Card = ({ index, card, onCardClick }) => {
-  const style = {};
+  const style = {
+    borderColor: card.revealed ? "coral" : "#999"
+  };
   return (
-    <div className="card" onClick={() => onCardClick(index)}>
-      <i className={card.icon} />
+    <div className="card" style={style} onClick={() => onCardClick(index)}>
+      <i
+        className={card.icon}
+        style={{ color: card.revealed ? "coral" : "#999" }}
+      />
     </div>
   );
 };
